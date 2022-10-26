4c4fc6719d03dc017c7ddee6317916016198035c
jennaliu <mla400@sfu.ca>
2022-10-26 16:12:57 -0700

    Initial commit of help activity

---
+++ app/src/main/java/ca/sfu/dba56/cmpt276/HelpActivity.java
@@ -0,0 +1,20 @@
+package ca.sfu.dba56.cmpt276;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+
+    public static Intent makeIntent(Context context){
+        return new Intent(context, HelpActivity.class);
+    }
+}
