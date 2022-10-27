0a9a9de508599ba3e1614345db446558688fc6ea
gurdaanm <gurdaanm@sfu.ca>
2022-10-26 17:20:01 -0700

    Add Achievements class
    getAchievement gets uses the range and player score to find what Achievement the player gets. Still need to figure out ranges.

---
+++ app/src/main/java/ca/sfu/dba56/cmpt276/model/Achievements.java
@@ -0,0 +1,49 @@
+package ca.sfu.dba56.cmpt276.model;
+
+public class Achievements {
+
+    /* This class uses the range of the game and the player score to find out what
+     * Achievement the player is.
+     */
+    public void getAchievement(int minScore, int maxScore, int playerScore){
+        String Achievement;
+        /*
+        if() {
+            Achievement = "Terrible Turtle";
+        }
+
+        if(){
+            Achievement = " Awful Alligator ";
+        }
+
+        if(){
+            Achievement = "Dancing Deer";
+        }
+
+        if(){
+            Achievement = "Cool Crocodile";
+        }
+
+        if(){
+            Achievement= "Fabulous Flamingo";
+        }
+
+        if(){
+            Achievement = "Majestic Mermaid";
+
+        }
+
+        if(){
+            Achievement = "Fierce Falcon";
+        }
+
+        if(){
+            Achievement = "Greatest Gorilla";
+        }
+
+        */
+    }
+
+
+
+}
