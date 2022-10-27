c512045c87f4d5ff03c382962d5caa7ae0fdc4ee
jennaliu <mla400@sfu.ca>
2022-10-26 21:45:21 -0700

    Implement an alertDialog to show achievement level

--- app/src/main/java/ca/sfu/dba56/cmpt276/AddNewGame.java
+++ app/src/main/java/ca/sfu/dba56/cmpt276/AddNewGame.java
@@ -2,13 +2,17 @@ package ca.sfu.dba56.cmpt276;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -19,6 +23,8 @@ public class AddNewGame extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_game);
         chooseGame();
+        saveInput();
+        showResult();
     }
 
     public static Intent makeIntent(Context context){
@@ -27,6 +33,7 @@ public class AddNewGame extends AppCompatActivity {
 
     private void chooseGame() {
         Spinner dropdown = findViewById(R.id.gameName);
+        // testing
         String[] items = new String[]{"Poker", "Bingo", "Wordle New"};
         // create an adapter to describe how the items are displayed
         // basic variant
@@ -39,6 +46,26 @@ public class AddNewGame extends AppCompatActivity {
     private void saveInput(){
         EditText num_player = findViewById(R.id.num_players_input);
         EditText combined_score = findViewById(R.id.combined_score_input);
+
+    }
+
+    private void showResult(){
+        Button check = findViewById(R.id.check_btn);
+        check.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(AddNewGame.this).create(); //Read Update
+                alertDialog.setTitle("Achievement");
+                alertDialog.setMessage("achievement level");
+
+                alertDialog.setButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(AddNewGame.this, MainActivity.class));
+                    }
+                });
+                alertDialog.show();
+            }
+        });
 
     }
 
