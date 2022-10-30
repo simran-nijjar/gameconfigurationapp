96c9de3efa20624e4aee269f86fb7e2285c96f8d
Gurdaan Mann <gurdaanm@sfu.ca>
2022-10-30 14:02:52 -0700

    add edit configuration class
    making class for edit configuration

---
+++ app/src/main/java/ca/sfu/dba56/cmpt276/EditConfiguration.java
@@ -0,0 +1,14 @@
+package ca.sfu.dba56.cmpt276;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditConfiguration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_configuration);
+    }
+}
