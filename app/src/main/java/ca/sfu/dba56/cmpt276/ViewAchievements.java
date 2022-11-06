6eb4ce3d0173ffb324056c11931351c03f3fb3b6
Simran Nijjar <simran.nijjar@hotmail.com>
2022-11-06 15:26:30 -0800

    User cannot enter more than one zero for number of players

--- app/src/main/java/ca/sfu/dba56/cmpt276/ViewAchievements.java
+++ app/src/main/java/ca/sfu/dba56/cmpt276/ViewAchievements.java
@@ -97,6 +97,11 @@ public class ViewAchievements extends AppCompatActivity {
         }
         @Override
         public void afterTextChanged(Editable s) {
+            numPlayersStr = numPlayers.getText().toString();
+            if (numPlayersStr.startsWith("0") && numPlayersStr.length() > 1){
+                numPlayers.setText("");
+                noAchievementsDisplayed.setText(R.string.invalid_players_msg);
+            }
         }
     };
 
