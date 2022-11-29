c804e79ee9596f43caaba35c03bbedda4656740e
Simran Nijjar <simran.nijjar@hotmail.com>
2022-11-28 23:05:20 -0800

    Add pop-window to show level names with numbers for each theme in achievement statistics

--- app/src/main/java/ca/sfu/dba56/cmpt276/AchievementStatistics.java
+++ app/src/main/java/ca/sfu/dba56/cmpt276/AchievementStatistics.java
@@ -1,13 +1,23 @@
 //Referenced https://www.youtube.com/watch?v=pi1tq-bp7uA and https://www.youtube.com/watch?v=H6QxMBI2QH4
 //Creating a Simple Bar Graph for your Android Application (part 1/2) and Creating a Simple Bar Graph for your Android Application (part 2/2) by CodingWithMitch
-//https://weeklycoding.com/mpandroidchart-documentation/
-//https://stackoverflow.com/a/69951831
+//Watched the above youtube videos to learn how to make a bar graph
+//https://weeklycoding.com/mpandroidchart-documentation/  (used this documentation to modify the graph)
+//https://stackoverflow.com/a/69951831 (used this to add repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS) and maven { url 'https://jitpack.io' } in settings.gradle
 
 package ca.sfu.dba56.cmpt276;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -31,6 +41,9 @@ public class AchievementStatistics extends AppCompatActivity {
     private int selectedGamePosition;
     private ArrayList<Integer> achievementLevelNumbers; //Arraylist of all of the achievement levels number values
     private ArrayList<BarEntry> timesLevelEarned; //Arraylist of the number of times user achieved a level for a game configuration
+    private PopupWindow popUpLevelNames;
+    private LayoutInflater inflater;
+    private View popUpView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +52,7 @@ public class AchievementStatistics extends AppCompatActivity {
         statsGraph = findViewById(R.id.stats_graph);
         selectedGamePosition = manager.getIndex();
         createAchievementStatisticsGraph();
+        setUpPopUpLevelNameButton();
     }
 
     public static Intent makeIntent(Context context){
@@ -54,13 +68,11 @@ public class AchievementStatistics extends AppCompatActivity {
         //The y-axis values for the times user has earned each level
         timesLevelEarned = new ArrayList<>();
         for (int i = 0; i < achievementLevelNumbers.size(); i++) {
-            //Levels start from 1 so start from (i+1)
-            //Second parameter gets the number of times user has achieved the level which is stored in an array specific to each game config.
             //If level is not earned at least once
             if (manager.getItemAtIndex(selectedGamePosition).getAchievementsEarnedStats(i) == 0){
                 //Don't add to graph
-            } else {
-                timesLevelEarned.add(new BarEntry(i + 1, manager.getItemAtIndex(selectedGamePosition).getAchievementsEarnedStats(i)));
+            } else { //Else, add number of each achievement level is earned to the level number
+                timesLevelEarned.add(new BarEntry((i + 1), manager.getItemAtIndex(selectedGamePosition).getAchievementsEarnedStats(i)));
             }
         }
         //Create the data set for the number of times user has earned each level
@@ -72,16 +84,93 @@ public class AchievementStatistics extends AppCompatActivity {
         Description description = new Description();
         description.setText("");
         statsGraph.setDescription(description);
+
         //x-axis modifications
         XAxis xAxis = statsGraph.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawAxisLine(true);
         xAxis.setGranularity(1f);
         xAxis.setGranularityEnabled(true);
+
         //y-axis modifications
         YAxis leftYAxis = statsGraph.getAxisLeft();
         leftYAxis.setGranularity(1f); //Increase by 1 for y-axis
         YAxis rightYAxis = statsGraph.getAxisRight();
         rightYAxis.setEnabled(false); //Remove right y-axis
+    }
+
+    //When user clicks button, pop-up window is displayed showing the levels for each theme with numbers
+    private void setUpPopUpLevelNameButton(){
+        Button levelBtn = findViewById(R.id.levelsBtn);
+        levelBtn.setOnClickListener(v -> {
+            //Display the view from layout file
+            inflater = LayoutInflater.from(AchievementStatistics.this);
+            popUpView = inflater.inflate(R.layout.pop_up_level_names, null);
+            popUpLevelNames = new PopupWindow(popUpView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, true);
+            popUpLevelNames.setAnimationStyle(android.R.style.Animation_Dialog);
+            popUpLevelNames.showAtLocation(popUpView, Gravity.CENTER, 0, 0);
+            popUpLevelNames.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+            //Buttons that when clicked in the pop-up window, will show levels for the clicked theme
+            setUpFruitLevelsButton();
+            setUpFantasyLevelsButton();
+            setUpStarWarsLevelsButton();
+            //Button to close the pop-up window
+            setUpClosePopUpButton();
+        });
+    }
+
+    private void setUpFruitLevelsButton(){
+        Button fruitsBtn = popUpView.findViewById(R.id.fruitsLevelsBtn);
+        TextView fruitsLevelsWithNum = popUpView.findViewById(R.id.fruitsLevelsWithNums);
+        fruitsLevelsWithNum.setVisibility(View.GONE);
+        fruitsBtn.setOnClickListener(v -> {
+            //Display levels when clicked
+            if(fruitsLevelsWithNum.getVisibility() == View.GONE) {
+                fruitsLevelsWithNum.setText(R.string.fruits_levels_with_num);
+                fruitsLevelsWithNum.setVisibility(View.VISIBLE);
+            } else if (fruitsLevelsWithNum.getVisibility() == View.VISIBLE){
+                fruitsLevelsWithNum.setText("");
+                fruitsLevelsWithNum.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setUpFantasyLevelsButton(){
+        Button fantasyBtn = popUpView.findViewById(R.id.fantasyLevelsBtn);
+        TextView fantasyLevelsWithNum = popUpView.findViewById(R.id.fantasyLevelsWithNum);
+        fantasyLevelsWithNum.setVisibility(View.GONE);
+        fantasyBtn.setOnClickListener(v -> {
+            //Display levels when clicked
+            if(fantasyLevelsWithNum.getVisibility() == View.GONE) {
+                fantasyLevelsWithNum.setText(R.string.fantasy_levels_with_num);
+                fantasyLevelsWithNum.setVisibility(View.VISIBLE);
+            } else if (fantasyLevelsWithNum.getVisibility() == View.VISIBLE){
+                fantasyLevelsWithNum.setText("");
+                fantasyLevelsWithNum.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setUpStarWarsLevelsButton(){
+        Button starwarsBtn = popUpView.findViewById(R.id.starwarsLevelsBtn);
+        TextView starwarsLevelsWithNum = popUpView.findViewById(R.id.starwarsLevelsWithNum);
+        starwarsLevelsWithNum.setVisibility(View.GONE);
+        starwarsBtn.setOnClickListener(v -> {
+            //Display levels when clicked
+            if(starwarsLevelsWithNum.getVisibility() == View.GONE) {
+                starwarsLevelsWithNum.setText(R.string.starwars_levels_with_num);
+                starwarsLevelsWithNum.setVisibility(View.VISIBLE);
+            } else if (starwarsLevelsWithNum.getVisibility() == View.VISIBLE){
+                starwarsLevelsWithNum.setText("");
+                starwarsLevelsWithNum.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void setUpClosePopUpButton(){
+        Button closeBtn = popUpView.findViewById(R.id.closeBtn);
+        closeBtn.setOnClickListener(v -> {
+            popUpLevelNames.dismiss();
+        });
     }
 }
